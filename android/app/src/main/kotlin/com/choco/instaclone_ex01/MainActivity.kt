cbc6ecdc521cd1740abf7c72d0d69e0d5633cfd4
shanghanrun <idim7@naver.com>
2024-01-18 00:41:41 +0900

    .

---
+++ android/app/src/main/kotlin/com/choco/instaclone_ex01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.choco.instaclone_ex01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:instaclone_ex01/body.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+            primary: Colors.white,
+            secondary: Colors.black,
+          ),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: Colors.black,
+          )),
+      home: const InstaHome(),
+    );
+  }
+}
+
+class InstaHome extends StatefulWidget {
+  const InstaHome({super.key});
+
+  @override
+  State<InstaHome> createState() => _InstaHomeState();
+}
+
+class _InstaHomeState extends State<InstaHome> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Instagram',
+            style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32)),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_outline, size: 32),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.paperplane, size: 32),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: InstaBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (newIndex) {
+          setState(() {
+            index = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 30), label: 'search'),
+        ],
+      ),
+    );
+  }
+}
